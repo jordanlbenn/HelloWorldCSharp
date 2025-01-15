3e4072cfa80d6030c2fd435dbfacc9c401478122
Jordan Bennett <156590653+jordanlbenn@users.noreply.github.com>
2025-01-14 19:45:18 -0500

    First version of Hello World
    A car object, and a form.

---
+++ HelloWorldCSharp/HelloWorldCSharp/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCSharp
+{
+    public class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+        public string VIN;
+        public string exteriorColor;
+        string interiorColor;
+        public Car(string make, string model, int year, string vIN, string exteriorColor, string interiorColr)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            VIN = vIN;
+            this.exteriorColor = exteriorColor;
+            this.interiorColor = interiorColor;
+        }
+    }
+}
